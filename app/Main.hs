3aece2c2664f4d24ca078fc6ad402ac8ba8e46d9
oldmanmike <oldmanmike.dev@gmail.com>
2015-10-21 14:07:00 -0400

    Fix Formatting

--- app/Main.hs
+++ app/Main.hs
@@ -2,18 +2,18 @@
 module Main where
 
 
-import            OpenSandbox
-import            OpenSandbox.Minecraft.Protocol
 import            Control.Concurrent
 import qualified  Data.Aeson as Aeson
-import qualified  Data.Text as T
-import            GHC.Generics
-import            Network.Socket hiding (send,recv)
-import            Network.Socket.ByteString
 import            Data.Binary
 import qualified  Data.ByteString as B
 import qualified  Data.ByteString.Lazy as BL
+import qualified  Data.Text as T
 import            Data.Word
+import            GHC.Generics
+import            Network.Socket hiding (send,recv)
+import            Network.Socket.ByteString
+import            OpenSandbox
+import            OpenSandbox.Minecraft.Protocol
 
 
 main :: IO ()
@@ -29,39 +29,42 @@ main = do
 
 mainLoop :: Socket -> IO ()
 mainLoop sock = do
-  conn <- accept sock
-  runConn conn
-  mainLoop sock
+    conn <- accept sock
+    runConn conn
+    mainLoop sock
+
 
 runConn :: (Socket, SockAddr) -> IO ()
 runConn (sock, _) = do
-  maybeHandshake <- recv sock 254
-  putStrLn "==================================================================="
-  putStrLn "|                    << Packet Report Begin >>                    |"
-  putStrLn "==================================================================="
-  putStrLn "[Raw Handshake]"
-  print maybeHandshake
-  print $ B.unpack maybeHandshake
-  putStrLn "==================================================================="
-  putStrLn "[Parsed]"
-  print $ (decode (BL.fromStrict maybeHandshake) :: ServerBoundHandshake)
-  let response = BL.toStrict (Aeson.encode testResponse)
-  let outgoing = B.cons (121 :: Word8) (B.cons (0 :: Word8) (B.cons (119 :: Word8) response))
-  send sock outgoing
-  putStrLn "==================================================================="
-  putStrLn "///////////////////////////////////////////////////////////////////"
-  putStrLn "==================================================================="
-  ping <- recv sock 254
-  putStrLn "[Raw Ping]"
-  print ping
-  print $ B.unpack ping
-  maybePing sock ping
-  putStrLn "==================================================================="
-  putStrLn "///////////////////////////////////////////////////////////////////"
-  putStrLn "==================================================================="
-  putStrLn "|                     << Packet Report End >>                     |"
-  putStrLn "==================================================================="
-  sClose sock
+    maybeHandshake <- recv sock 254
+    putStrLn "================================================================="
+    putStrLn "|                   << Packet Report Begin >>                   |"
+    putStrLn "================================================================="
+    putStrLn "[Raw Handshake]"
+    print maybeHandshake
+    print $ B.unpack maybeHandshake
+    putStrLn "================================================================="
+    putStrLn "[Parsed]"
+    print $ (decode (BL.fromStrict maybeHandshake) :: ServerBoundHandshake)
+    let response = BL.toStrict (Aeson.encode testResponse)
+    let outgoing = B.cons (121 :: Word8)
+                          (B.cons (0 :: Word8)
+                          (B.cons (119 :: Word8) response))
+    send sock outgoing
+    putStrLn "================================================================="
+    putStrLn "/////////////////////////////////////////////////////////////////"
+    putStrLn "================================================================="
+    ping <- recv sock 254
+    putStrLn "[Raw Ping]"
+    print ping
+    print $ B.unpack ping
+    maybePing sock ping
+    putStrLn "================================================================="
+    putStrLn "/////////////////////////////////////////////////////////////////"
+    putStrLn "================================================================="
+    putStrLn "|                    << Packet Report End >>                    |"
+    putStrLn "================================================================="
+    sClose sock
 
 
 maybePing :: Socket -> B.ByteString -> IO ()
@@ -77,7 +80,6 @@ maybePing sock maybePing = if ((B.length maybePing == 10) && ((B.index maybePing
 
 testResponse :: Response
 testResponse = Response
-  (Version "15w42a" 79)
-  (Players 20 0)
-  (Description "A Minecraft Server")
-
+    (Version "15w42a" 79)
+    (Players 20 0)
+    (Description "A Minecraft Server")
